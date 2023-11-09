0d0040ca55fcbb706c4fa8ee9f5dad9f6ff10ef0
Documenter.jl <documenter@juliadocs.github.io>
2023-11-09 12:56:14 +0000

    build based on f76fe85

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Scaling","category":"page"},{"location":"#Scaling","page":"Home","title":"Scaling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Scaling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Scaling]","category":"page"}]
+}
